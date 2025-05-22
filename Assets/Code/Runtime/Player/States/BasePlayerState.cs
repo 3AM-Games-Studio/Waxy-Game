3c7f31f90a3d5c6bfc7111a564988cd778384e60
LucaValentini25 <139935542+LucaValentini25@users.noreply.github.com>
2025-05-22 19:20:38 -0300

    New character controller test (#22)
    * Import de Waxy y test de nuevo movimiento
    
    * implementacion de waxy y cambio de stats
    
    * Inicio de organizacion de objetos
    
    Objetos importados:
    -Estantes
    -Libros
    
    * Importación de objetos
    
    Objetos importados:
    -Pociones
    -Caldero
    -Lampara de aceite
    -Telaraña
    -Cartas
    
    * Importación de objetos
    
    Objetos importados:
    -Muchos
    
    * Importación de materiales
    
    Materiales importados:
    -Paredes de la azotea
    -Paredes de la libreria
    -Piso de la azotea
    -Agua del caldero
    
    * Scale factor de objetos
    
    * Meshes con la escala arreglada
    
    -Hojas decorativas
    -Alfombra circular
    
    * [F] Delete duplicate player inputs & waxy mesh
    
    * Importación de particulas
    
    * Prefabs deco e interactive
    
    * Particulas a VFX
    
    * NPC Base
    
    * NPC_Base_Deco y NPC_Base_Interactive
    
    * Base Player Controller
    
    * Running & Sliding
    
    ---------
    
    Co-authored-by: GyanFM <gianfrasettem@gmail.com>
    Co-authored-by: SantiagoBocco <santyynup@gmail.com>

---
+++ Assets/Code/Runtime/Player/States/BasePlayerState.cs
@@ -0,0 +1,144 @@
+﻿using AdvancedController;
+using UnityEngine;
+using UnityUtils.StateMachine;
+
+namespace Player.States
+{
+    public abstract class BasePlayerState : IState
+    {
+        protected readonly PlayerController controller;
+        protected AnimationController animationController;
+        public BasePlayerState(PlayerController controller)
+        {
+            this.controller = controller;
+            animationController = controller.GetComponent<AnimationController>();
+        }
+
+        public virtual void Update() { }
+        public virtual void FixedUpdate() { }
+
+        public virtual void OnEnter() { }
+        public virtual void OnExit() { }
+    }
+    //Player States Idle, Move, Running, Jumping, Falling, Sliding, Grounded, Carrying, Pushing, Interacting, Dead
+    public class IdleState : BasePlayerState
+    {
+        public IdleState(PlayerController controller) : base(controller) { }
+
+        public override void FixedUpdate()
+        {
+            controller.MovementModule.ApplyIdleFriction();
+        }
+    }
+
+    
+    public class MoveState : BasePlayerState
+    {
+        public MoveState(PlayerController controller) : base(controller)
+        {
+        }
+
+        public override void FixedUpdate()
+        {
+            controller.MovementModule.ApplyGroundedMovement();
+        }
+    }
+    public class RunningState : BasePlayerState
+    {
+        public RunningState(PlayerController controller) : base(controller)
+        {
+        }
+
+        public override void FixedUpdate()
+        {
+            controller.MovementModule.ApplyGroundedMovement();
+        }
+    }
+    public class JumpingState : BasePlayerState
+    {
+        public JumpingState(PlayerController controller) : base(controller)
+        {
+        }
+        
+        public override void OnEnter() {
+            controller.MovementModule.OnJumpStart();
+        }
+        
+        public override void FixedUpdate() => controller.MovementModule.ApplyAirMovement();
+        public override void OnExit()
+        {
+            // animationController.HandleJump(false);
+        }
+    }
+    public class FallingState : BasePlayerState
+    {
+        public FallingState(PlayerController controller) : base(controller)
+        {
+        }
+        public override void OnEnter() => controller.MovementModule.OnFallStart();
+        
+        public override void FixedUpdate() => controller.MovementModule.ApplyAirMovement();
+    }
+    public class RisingState : BasePlayerState {
+
+        public RisingState(PlayerController controller) : base(controller) { }
+
+        public override void OnEnter()
+        {
+        }
+
+        public override void FixedUpdate() => controller.MovementModule.ApplyAirMovement();
+    }
+
+    public class SlidingState : BasePlayerState
+    {
+        public SlidingState(PlayerController controller) : base(controller)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log("Enter Sliding State");
+        }
+        public override void FixedUpdate()
+        {
+            controller.MovementModule.ApplySlideMovement();
+        }
+    }
+    public class GroundedState : BasePlayerState
+    {
+        public GroundedState(PlayerController controller) : base(controller)
+        {
+        }
+        public override void OnEnter()
+        {
+            base.OnEnter(); 
+            // controller.MovementModule.OnGroundContactRegained();
+        }
+    }
+    public class CarryingState : BasePlayerState
+    {
+        public CarryingState(PlayerController controller) : base(controller)
+        {
+        }
+    }
+    public class PushingState : BasePlayerState
+    {
+        public PushingState(PlayerController controller) : base(controller)
+        {
+        }
+    }
+    
+    public class InteractingState : BasePlayerState
+    {
+        public InteractingState(PlayerController controller) : base(controller)
+        {
+        }
+    }
+    public class DeadState : BasePlayerState
+    {
+        public DeadState(PlayerController controller) : base(controller)
+        {
+        }
+    }
+}
